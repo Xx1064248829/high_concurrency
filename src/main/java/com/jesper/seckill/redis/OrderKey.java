312455dfcdb91bce6486ebe8993bc1506f6c49c1
Xx、 <1064248829@qq.com>
2020-01-22 23:24:58 +0800

    秒杀系统代码提交

---
+++ src/main/java/com/jesper/seckill/redis/OrderKey.java
@@ -0,0 +1,11 @@
+package com.jesper.seckill.redis;
+
+
+public class OrderKey extends BasePrefix {
+
+    public OrderKey(String prefix) {
+        super(prefix);
+    }
+
+    public static OrderKey getSeckillOrderByUidGid = new OrderKey("seckill");
+}
